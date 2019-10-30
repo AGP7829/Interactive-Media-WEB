00ee51828eeebd3b7335902173a3cdba9db7b986
Alleesa <alleesaparagas135@gmail.com>
2019-10-30 11:48:31 -0400

    update

--- ACT-02/practice/js/practice.js
+++
@@ -1,7 +0,0 @@
-
-function myFunction()
-{
-	var num1= parseInt(document.getElementById("num1").value);
-	var num2= parseInt(document.getElementById("num2").value);
-	document.getElementById("output").innerHTML = num1+num2;
-}
